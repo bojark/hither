338c6f377be407a1580fd23710b841dea66f9f7c
Ivan Kostin <kosstin@gmail.com>
2026-07-27 16:53:23 +0400

    Add project structure, world.gd script

--- scripts/core/game.gd
+++ scripts/core/game.gd
@@ -1,6 +1,8 @@
 extends Node
 
+var world: World
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("=== Hither started ===")
+	world = World.new()
---
+++ scripts/core/world.gd
@@ -0,0 +1,6 @@
+extends Node
+
+class_name World
+
+func _init() -> void:
+	print("World created")
